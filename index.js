4e220123db8974d6f4e5e940c989d479fbad6d0c
Blake La Pierre <blakelapierre@gmail.com>
2015-02-27 04:15:58 -0800

    This works, allowing different module name.

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-// module.exports = function(gulp, tasks) {
-//   for (var key in tasks) {
-//     tasks[key](gulp);
-//   }
-//   return tasks;
-// };
-
-module.exports = function(gulp, require) {
-  var tasks = require('./tasks');
+module.exports = function(gulp, require, tasksModule) {
+  var tasks = require(tasksModule || './tasks');
   for (var key in tasks) {
     tasks[key](gulp);
   }
